dc649df9f8003fefada661c7a661c14ee3930367
Ghostgirl05 <gcherotich05@gmail.com>
2022-07-05 12:31:40 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,40 +2,28 @@
 
 /**
  * times_table - prints the 9 times table
- * Description: prints the 9 times table
- * Returns: void
+ *
+ * Return: returns nothing
  */
-
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int digit, mult, result;
 
-	for (row = 0; row <= 9; column++)
+	for (dgit = 0; digit <= 9; digit++)
 	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
+		_putchar('0');
 
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = digit * mult;
+			if (result <= 9)
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
-			}
 			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
-			}
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
